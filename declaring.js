ab7e8e89c6a9012871afe96049183db65a945fba
dncworking <dominyka.dobuzinskaite@gmail.com>
2025-11-26 13:04:41 +0200

    declaring.js / work done

---
+++ declaring.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let myArray = [
+  "sunday",
+  "monday",
+  "tuesday",
+  "wednesday",
+  "thursday",
+  "friday",
+  "saturday",
+];
+console.log(myArray[2]);
+myArray[4] = null;
+console.log(myArray.indexOf(null));
